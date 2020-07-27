2cd6c4025479cb62796897c0d207829dd74a878a
Asfar01 <asfar.tariq91@gmail.com>
2020-07-27 13:01:29 +0500

    Heroku2

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-var express = require("express");
-var socket = require("socket.io");
-var router = express.Router();
+// var express = require("express");
+// var socket = require("socket.io");
+// var router = express.Router();
 var serv = require("../bin/www");
+require=require('esm')(module)
 // var http = require('http');
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.status(200).send(`<h1>Express server is running</h1>`);
-});
+// router.get("/", function (req, res, next) {
+//   res.status(200).send(`<h1>Express server is running</h1>`);
+// });
 
-module.exports = {router, serv};
+module.exports = serv;
